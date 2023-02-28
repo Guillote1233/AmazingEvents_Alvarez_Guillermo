9d65f7a15bf2a3740e0527d17e8c6ab14a5ce384
Guillote1233 <98848165+Guillote1233@users.noreply.github.com>
2023-02-28 19:52:50 -0300

    dynamic filter categories

--- assets/js/past_events.js
+++ assets/js/past_events.js
@@ -196,4 +196,24 @@ function createCard(array, container){
     container.appendChild(fragment);
 }
 
+function createCategoryFilter(){
+    const categories = data.events.map(cat => cat.category);
+    const finalCategories = categories.filter((item, index) => {
+        return categories.indexOf(item) === index
+    }); 
+    
+    let catContainer = document.getElementById('filterCat');
+    const catFragment = document.createDocumentFragment();
+    
+    for(let items of finalCategories){
+        let label = document.createElement('label')
+        label.textContent = items
+        label.innerHTML += `<input type="checkbox" name="cat" id=${items.split(" ").join("").toLowerCase()}>`
+        catFragment.appendChild(label);
+    }
+    
+    catContainer.appendChild(catFragment);
+}
+
 createCard(data.events, cardContainer);
+createCategoryFilter();
