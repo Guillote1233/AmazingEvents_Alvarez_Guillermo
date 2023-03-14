245146d20d0778a84bef5d9cfc643818e0073774
Guillote1233 <98848165+Guillote1233@users.noreply.github.com>
2023-03-14 18:37:50 -0300

    details data

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,22 +1,47 @@
-import data from './data.js'
+const loader = document.getElementById('loader');
 
+const showLoader = () => {
+    loader.classList.add('spinner-border');
+};
+
+const hideLoader = () => {
+    loader.classList.remove('spinner-border');
+};
+
+let data = [];
+
+async function getData() {
+    try {
+        const apiUrl = "../assets/js/amazing.json";
+        const response = await fetch(apiUrl);
+        const json = await response.json();
+        data = json;
+        hideLoader();
+        const eventDetail = data.events.find(item => item._id == id);
+        const div = document.querySelector('.detailContainer');
+        div.innerHTML = `
+        <div class="imgContainer">
+        <img src="${eventDetail.image}" alt="img${eventDetail.name}">
+        </div>
+        <div class="descriptionContainer">
+        <h2>${eventDetail.name}</h2>
+        <h5>${eventDetail.category}</h5>
+        <p>${eventDetail.description}</p>
+        <div class="dateContainer">
+        <h5>Date: ${eventDetail.date}</h5>
+        <h5>Capacity: ${eventDetail.capacity}</h5>
+        </div>
+        </div>`
+    } catch (error) {
+        console.log(error);
+    }
+}
 const queryString = location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get('id');
 
-const eventDetail = data.events.find(item => item._id == id);
+showLoader();
+getData();
 
-const div = document.querySelector('.detailContainer');
-div.innerHTML = `
-<div class="imgContainer">
-<img src="${eventDetail.image}" alt="img${eventDetail.name}">
-</div>
-<div class="descriptionContainer">
-<h2>${eventDetail.name}</h2>
-<h5>${eventDetail.category}</h5>
-<p>${eventDetail.description}</p>
-<div class="dateContainer">
-<h5>Date: ${eventDetail.date}</h5>
-<h5>Capacity: ${eventDetail.capacity}</h5>
-</div>
-</div>`
+
+
