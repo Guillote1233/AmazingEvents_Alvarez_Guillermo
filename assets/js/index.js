6d3c19fbcb93859578c7ac9cf8f676fe997684e8
Guillote1233 <98848165+Guillote1233@users.noreply.github.com>
2023-03-18 14:38:39 -0300

    fixes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,47 +1,47 @@
-const cardContainer = document.getElementById('eventCard');
+const cardContainer = document.getElementById("eventCard");
 const fragment = document.createDocumentFragment();
-const catCheckbox = document.getElementById('filterCat');
+const catCheckbox = document.getElementById("filterCat");
 const searchValue = document.querySelector('input[placeholder="Search..."]');
-const backgroundLoader = document.getElementById('back-loader');
-const loader = document.getElementById('spinner');
+const backgroundLoader = document.getElementById("back-loader");
+const loader = document.getElementById("spinner");
 
 const showLoader = () => {
-    backgroundLoader.classList.remove('d-none');
-    loader.classList.add('spinner-border');
+  backgroundLoader.classList.remove("d-none");
+  loader.classList.add("spinner-border");
 };
 
 const hideLoader = () => {
-    backgroundLoader.classList.add('d-none')
-    loader.classList.remove('spinner-border');
+  backgroundLoader.classList.add("d-none");
+  loader.classList.remove("spinner-border");
 };
 
 let data = [];
 let categories = [];
 
 async function getData() {
-    try {
-        const apiUrl = "../assets/js/amazing.json";
-        const response = await fetch(apiUrl);
-        const json = await response.json();
-        data = json;
-        hideLoader();
-        createCard(data.events, cardContainer);
-        categories = createCategoryList(data.events);
-        createCheckboxFilter(categories, catCheckbox);
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const apiUrl = "../assets/js/amazing.json";
+    const response = await fetch(apiUrl);
+    const json = await response.json();
+    data = json;
+    hideLoader();
+    createCard(data.events, cardContainer);
+    categories = createCategoryList(data.events);
+    createCheckboxFilter(categories, catCheckbox);
+  } catch (error) {
+    console.log(error);
+  }
 }
 showLoader();
 getData();
 
-function createCard(array, container){
-    container.innerHTML = ""
-    if(array.length > 0){
-        array.forEach(items => {
-            let div = document.createElement("div")
-            div.className = "card col-lg-2 col-md-3 col-sm-12"
-            div.innerHTML += `
+function createCard(array, container) {
+  container.innerHTML = "";
+  if (array.length > 0) {
+    array.forEach((items) => {
+      let div = document.createElement("div");
+      div.className = "card col-lg-2 col-md-3 col-sm-12";
+      div.innerHTML += `
                 <img src="${items.image}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${items.name}</h5>
@@ -50,68 +50,73 @@ function createCard(array, container){
                         <h6>$ ${items.price}</h6>
                         <a href="../pages/details.html?id=${items._id}" class="btn btn-primary">Details</a>
                     </div>
-                </div>`
-            fragment.appendChild(div);
-            
-        });
-    }else{
-        container.innerHTML = `<img src="../assets/img/notfound.gif" alt="not-found">`;
+                </div>`;
+      fragment.appendChild(div);
+    });
+  } else {
+    container.innerHTML = `<img src="../assets/img/notfound.gif" alt="not-found">`;
+  }
+  container.appendChild(fragment);
+}
+
+function createCategoryList(array) {
+  let categories = array.map((cat) => cat.category);
+  categories = categories.reduce((accum, elem) => {
+    if (!accum.includes(elem)) {
+      accum.push(elem);
     }
-    container.appendChild(fragment);
+    return accum;
+  }, []);
+  return categories;
 }
 
-function createCategoryList(array){
-    let categories = array.map(cat => cat.category);
-    categories = categories.reduce((accum, elem) => {
-        if(!accum.includes(elem)){
-            accum.push(elem);
-        }
-        return accum;
-    },[])
-        return categories;
+function createCheckboxFilter(array, container) {
+  array.forEach((category) => {
+    let div = document.createElement("div");
+    div.className = `checkbox-container ${category.toLowerCase()}`;
+    div.innerHTML += `<input type="checkbox" id=${category.toLowerCase()} name="category">
+        <label for="${category.toLowerCase()}">${category}</label>`;
+    container.appendChild(div);
+  });
 }
 
-function createCheckboxFilter(array, container){
-    array.forEach(category => {
-        let div = document.createElement('div');
-        div.className = `checkbox-container ${category.toLowerCase()}`
-        div.innerHTML += `<input type="checkbox" id=${category.toLowerCase()} name="category">
-        <label for="${category.toLowerCase()}">${category}</label>`
-        container.appendChild(div);
-    })
+function searchFilter(array, value) {
+  let filterData = array.filter((elem) =>
+    elem.name.toLowerCase().includes(value.toLowerCase())
+  );
+  return filterData;
 }
 
-function searchFilter(array, value){
-    let filterData = array.filter(elem => elem.name.toLowerCase().includes(value.toLowerCase()));
-    return filterData;
-}
+function checkboxFilter(array) {
+  let checked = document.querySelectorAll('input[type="checkbox"]:checked');
+  let filteredList = [];
 
-function checkboxFilter(array){
-    let checked = document.querySelectorAll('input[type="checkbox"]:checked');
-    let filteredList = [];
-
-    if(checked.length > 0){
-        for(let i=0; i < checked.length; i++){
-            filteredList = filteredList.concat(array.filter(elem => elem.category.toLowerCase().includes(checked[i].id.toLowerCase())))
-        }
-    }else{
-        filteredList = array;
+  if (checked.length > 0) {
+    for (let i = 0; i < checked.length; i++) {
+      filteredList = filteredList.concat(
+        array.filter((elem) =>
+          elem.category.toLowerCase().includes(checked[i].id.toLowerCase())
+        )
+      );
     }
-    return filteredList;
+  } else {
+    filteredList = array;
+  }
+  return filteredList;
 }
 
-function filterFinal(array){
-    let arrayFiltered = searchFilter(array, searchValue.value);
-    arrayFiltered = checkboxFilter(arrayFiltered);
-    return arrayFiltered;
+function filterFinal(array) {
+  let arrayFiltered = searchFilter(array, searchValue.value);
+  arrayFiltered = checkboxFilter(arrayFiltered);
+  return arrayFiltered;
 }
 
-searchValue.addEventListener('keyup', () => {
-    let dataFilter = filterFinal(data.events)
-    createCard(dataFilter, cardContainer)
-})
+searchValue.addEventListener("keyup", () => {
+  let dataFilter = filterFinal(data.events);
+  createCard(dataFilter, cardContainer);
+});
 
-catCheckbox.addEventListener('change', ()=> {
-    let dataFilter = filterFinal(data.events)
-    createCard(dataFilter, cardContainer)
-})
+catCheckbox.addEventListener("change", () => {
+  let dataFilter = filterFinal(data.events);
+  createCard(dataFilter, cardContainer);
+});
